a24e0bdbdf1327cae62659447a0eac1de583b139
Disertel <45638607+Disertel@users.noreply.github.com>
2018-12-12 18:02:17 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'demo',
+		wp_ws_url : 'http://app.disertel.com/wp-appkit-api/demo',
+		wp_url : 'http://app.disertel.com',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'Demo app',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'YLp)oN,5TnZ|K;_<.#{vrzQFgMAyI)Y>ddRF5y]N1D$a=!LDUyGfhKFBu5gvGq?4',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
